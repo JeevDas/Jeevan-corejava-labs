677dcb71d848c7fe0df8a7ee2178017d5b50fff4
JeevDas <92858739+JeevDas@users.noreply.github.com>
2021-11-11 18:05:09 +0530

    commit
    commit

---
+++ src/Lab1/q8/Exercise8.java
@@ -0,0 +1,27 @@
+package Lab1.q8;
+
+public class Exercise8 {
+	public static boolean checkNumber(int n)
+	 {
+	 while(n%2==0)
+	 {
+	 n=n/2;
+	 }
+	 if(n==1)
+	 {
+	 return true;
+	 }
+	 else
+	 {
+	 return false;
+	 }
+	 }
+
+	public static void main(String[] args) {
+System.out.println(checkNumber(6));
+	}
+
+}
+
+
+
